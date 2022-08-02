2dcdc7d5ad6acece7f843b5b1883dd11e7439588
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-08-03 01:13:25 +0530

    solid rhombus

--- FirstTime/Patterns/patterns.java
+++ FirstTime/Patterns/patterns.java
@@ -105,3 +105,21 @@ class Butterfly{
         }
     }
 }
+
+class SolidRhombus{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=2*n-1; j++){
+                if(j>=n-i+1 && j<=2*n-i){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
