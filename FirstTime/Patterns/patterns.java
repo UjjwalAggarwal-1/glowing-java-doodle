8c3d07f27bdd1f35fcdfcf1dec94e44dabf33392
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-08-03 00:43:27 +0530

    Butterfly

--- FirstTime/Patterns/patterns.java
+++ FirstTime/Patterns/patterns.java
@@ -87,3 +87,21 @@ class FloydsTriangle{
         }
     }
 }
+
+class Butterfly{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=2*n;i++){
+            for(int j=1;j<=2*n;j++)
+                if(i<=n && (j<=i || j>=2*n+1-i)){
+                    System.out.print("* ");
+                }else if(i>n &&(j<=2*n+1-i || j>=i)){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+                System.out.println();
+        }
+    }
+}
